3352de836a2cffee15db0c5a36abaa007babfb9c
BhoomikaT <BhoomikaT@DEV038IN>
2020-03-11 19:04:59 +0530

    Add project files.

---
+++ Time machine/Class1.cs
@@ -0,0 +1,31 @@
+﻿using OpenQA.Selenium.Remote;
+using OpenQA.Selenium.Winium;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Time_machine
+{
+    class Class1
+    {
+        //private static void Main()
+      //  {
+           
+
+          /*  var window = driver.FindElementByClassName("CalcFrame");
+            var viewMenuItem = window.FindElement(By.Id("MenuBar")).FindElement(By.Name("View"));
+            viewMenuItem.Click();
+            viewMenuItem.FindElement(By.Name("Scientific")).Click();
+            viewMenuItem.Click();
+            viewMenuItem.FindElement(By.Name("History")).Click();
+            window.FindElement(By.Id("132")).Click(); // 2
+            window.FindElement(By.Id("93")).Click(); // +
+            window.FindElement(By.Id("134")).Click(); // 4
+            window.FindElement(By.Id("97")).Click(); // ^
+            window.FindElement(By.Id("138")).Click(); // 8
+            window.FindElement(By.Id("121")).Click(); // =
+            */
+           // driver.Close();
+      //  }
+    }
+}
---
+++ Time machine/Program.cs
@@ -0,0 +1,23 @@
+﻿using Flurl;
+using OpenQA.Selenium.Remote;
+using OpenQA.Selenium.Winium;
+using System;
+
+namespace Time_machine
+{
+    class Program
+    {
+        public static DesiredCapabilities WINIUM_CAPABILITIES { get; private set; }
+        public static char ICommandExecutor { get; private set; }
+        public static DesiredCapabilities DesiredCapabilities { get; private set; }
+
+        static void Main(string[] args)
+        {
+            
+            var dc = new DesiredCapabilities();
+            dc.SetCapability("app", @"\\deimos.contrado.local\Users\bhoomikat\Desktop\Application Files\ContradoStaff_1_0_0_6\ContradoStaff.exe");
+            var driver = new RemoteWebDriver(new Uri("http://localhost:9999"), dc);
+        }
+    }
+}
+
