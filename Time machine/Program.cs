1bc36a16f016ea3d9a1126ad98bf0495c0c3298f
BhoomikaT <BhoomikaT@DEV038IN>
2020-03-12 15:11:23 +0530

    trsdfdxsf

--- Time machine/Program.cs
+++ Time machine/Program.cs
@@ -1,7 +1,10 @@
 ﻿using Flurl;
+using java.awt;
+using OpenQA.Selenium.Interactions;
 using OpenQA.Selenium.Remote;
 using OpenQA.Selenium.Winium;
 using System;
+
 
 namespace Time_machine
 {
@@ -13,11 +16,22 @@ namespace Time_machine
 
         static void Main(string[] args)
         {
-            
+
             var dc = new DesiredCapabilities();
             dc.SetCapability("app", @"\\deimos.contrado.local\Users\bhoomikat\Desktop\Application Files\ContradoStaff_1_0_0_6\ContradoStaff.exe");
-            var driver = new RemoteWebDriver(new Uri("http://localhost:9999"), dc);
 
+            var driver = new RemoteWebDriver(new Uri("http://localhost:9999"), dc,TimeSpan.FromMinutes(2));
+
+          //  String appPath = "\\deimos.contrado.local\Users\bhoomikat\Desktop\Application Files\ContradoStaff_1_0_0_6\ContradoStaff.exe";
+
+            //driver.FindElementByName("Run").Click();
+            // Robot robot= new Robot();
+            // System.Threading.Thread.Sleep(7000);
+            //robot.keyPress(keycode: java.awt.@event.KeyEvent.VK_LEFT);
+            //  robot.keyPress(keycode: java.awt.@event.KeyEvent.VK_ENTER);
+
+            //driver.FindElementByClassName("Button").Click();
+            driver.FindElementById("2").Click();
         }
     }
 }
